95043d12b71e3633e7190ad22dbb5c99498ff7e4
Josh Endemann <jahend44@gmail.com>
2023-07-08 12:54:41 -0500

    add html, css, and javascript to blackjack game

---
+++ index.js
@@ -0,0 +1,19 @@
+let firstCard = 9
+let secondCard = 6
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭";
+    }
+    // Cash Out!
+    console.log(message);
+}
