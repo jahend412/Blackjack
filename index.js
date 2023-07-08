065f55165cdf919785178acde158281bbdb7ed5b
Josh Endemann <jahend44@gmail.com>
2023-07-08 16:42:42 -0500

    Create getRandomCard function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 11
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard] // Array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,13 +9,31 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("card-el");
 
+//Create a function for Random Card
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13 + 1);
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
+}
+
+// Create a new function called startGame() that calls renderGame()
 function startGame() {
     renderGame();
 }
 
 function renderGame() {
     // Render out all of the cards we have
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    cardsEl.textContent = "Cards: ";
+    // for loop to render cards
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+    // Render out the sum
     sumEl.textContent = "Sum: " + sum;
 
     if (sum <= 20) {
@@ -32,8 +50,9 @@ function renderGame() {
 
 function newCard() {
     console.log("Drawing a new card from the deck!");
-    let card = 8;
+    let card = getRandomCard();
     sum += card;
+    //Push the card to the cards array
     cards.push(card);
     renderGame();
 }
