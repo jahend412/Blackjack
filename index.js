02be8a40098549541a27b9fd59a4a48a8eec24bb
Josh Endemann <jahend44@gmail.com>
2023-07-08 13:46:18 -0500

    added message, sum, and card elements

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-let firstCard = 9
-let secondCard = 6
+let firstCard = 10
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el");
+console.log(messageEl)
+
+let sumEl = document.getElementById("sum-el");
+console.log(sumEl)
+
+let cardEl = document.getElementById("card-el");
+
 function startGame() {
+    cardEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
+
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂";
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
+        message = "Wohoo! You've got Blackjack!";
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭";
+        message = "You're out of the game!";
     }
     // Cash Out!
+    messageEl.textContent = message;
     console.log(message);
 }
